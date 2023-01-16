2608f099678c7d4917b41c58c9b10b920b18da17
noob_coder <shibamdebnath07@gmail.com>
2023-01-17 01:34:41 +0530

    expense model

---
+++ models/expense.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new SchemaTypes({
+  id: {
+    type: String,
+    require: true,
+  },
+  amount: {
+    type: String,
+    require: true,
+  },
+  notes: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  split_method: {
+    type: String,
+    enum: ["equally", "amounts"],
+  },
+  split_between: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+      paid: {
+        type: Number,
+      },
+    },
+  ],
+});
