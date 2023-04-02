abd2c0a15a2d506905b74506719cee6d4b6a0a6c
SHIBAM <shibamdebnath07@gmail.com>
2023-04-03 01:44:30 +0530

    minor update

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { getGroup, createGroup, addusers, inviteUserInGroup, deleteUserFromGroup, deleteGroup, editGroupName, getAllGroups, getAllUserOfCurrentGroup, getPreviousGroups, settleExpenses, settled, deletePreviouGroup } = require('../controllers/group');
-const upload = require('../middleware/upload');
+const {
+  getGroup,
+  createGroup,
+  addusers,
+  inviteUserInGroup,
+  deleteUserFromGroup,
+  deleteGroup,
+  editGroupName,
+  getAllGroups,
+  getAllUserOfCurrentGroup,
+  getPreviousGroups,
+  settleExpenses,
+  settled,
+  deletePreviouGroup,
+} = require("../controllers/group");
+const upload = require("../middleware/upload");
 const { route } = require("./userRoutes");
 
-// router.get("/", (req, res) => {
-//   res.send("<h1>Group</h1>");
-// });
+router.get("/", (req, res) => {
+  res.send("<h1>This is the api for group details</h1>");
+});
 
-router.post("/creategroup", upload.single('groupIcon'), createGroup);  // create group
+router.post("/creategroup", upload.single("groupIcon"), createGroup); // create group
 router.delete("/:groupid", deleteGroup);
 router.patch("/:groupid", editGroupName);
 router.get("/details/:groupId", getGroup);
@@ -23,8 +37,7 @@ router.get("/:groupid/users", getAllUserOfCurrentGroup);
 router.get("/:userid/previousgroups", getPreviousGroups);
 router.delete("/:userid/:groupid/previousgroup", deletePreviouGroup);
 
-router.post("/isSettled/:groupId/:isSettled",settled);
+router.post("/isSettled/:groupId/:isSettled", settled);
 router.get("/settle/:groupId", settleExpenses);
 
 module.exports = router;
-
